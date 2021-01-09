3400114dbf32d7918430e8b7eaef8e8c467aefbf
Md Salman <md.salman@northsouth.edu>
2021-01-09 19:37:04 +0600

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import useApiRequest from "./useApiRequest";
+
 
 function App() {
+  const url = `https://randomuser.me/api/?results=15`;
+
+  const {error, isLoaded, data} = useApiRequest(url);
+  console.log(data.results[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"></div>
   );
 }
 
